957b829196e04088f4161f867d3f7aab2386f4ca
fbc623 <fbc623@gmail.com>
2019-08-24 22:49:09 -0300

    feat: adicionado watson natural language understanting service

--- robots/text.js
+++ robots/text.js
@@ -1,11 +1,25 @@
 const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+
 const sentenceBoundaryDetection = require('sbd')
+
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+var NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js');
+
+var nlu = new NaturalLanguageUnderstandingV1({
+  iam_apikey: watsonApiKey,
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+});
+
+
 
 async function robot(content) {
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
+    limiMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
@@ -55,6 +69,42 @@ async function robot(content) {
           })
         })
       }
+
+      function limiMaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+          
+      }
+
+      async function fetchKeywordsOfAllSentences(content) {
+        for (const sentence of content.sentences) {
+          sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+
+      }
+      
+      async function fetchWatsonAndReturnKeywords(sentence) {
+        return new Promise((resolve, reject) => {
+          nlu.analyze({
+            text: sentence,
+            features: {
+              keywords: {}
+            }
+          }, (error, response) => {
+            if (error) {
+              reject(error)
+              return
+            }
+    
+            const keywords = response.keywords.map((keyword) => {
+              return keyword.text
+            })
+    
+            resolve(keywords)
+          })
+        })
+      } 
+
+      
     
 }
 
